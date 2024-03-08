898b1c5fa7487ef9f3705b77a5e75caceaf71fcb
Marck-road <109259291+Marck-road@users.noreply.github.com>
2024-03-08 20:43:20 +0800

    Pagwapa Landing Screen

--- javascript/javascript.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("sdg1").addEventListener("click", function() {
-    window.location.href = "pages/case_study_1.html";
-});
---
+++ public/javascript/javascript.js
@@ -0,0 +1,15 @@
+document.getElementById("sdg1").addEventListener("click", function() {
+    window.location.href = "public/pages/case_study_1.html";
+});
+
+
+$(document).ready(function () {
+    // Add click event listener to all nav links
+    $('#myNavbar .nav-link').on('click', function () {
+        // Remove 'active' class from all nav items
+        $('#myNavbar .nav-item').removeClass('active');
+        
+        // Add 'active' class to the clicked nav item
+        $(this).closest('.nav-item').addClass('active');
+    });
+});
